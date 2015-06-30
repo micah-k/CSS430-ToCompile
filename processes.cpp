404ae0f364ba2a1ece63e410f7268860042aac20
Micah Kelly <micahk@seank.com>
2015-06-29 19:57:14 -0700

    Found the error.

--- processes.cpp
+++ processes.cpp
@@ -19,26 +19,26 @@ int main(int argc, char* argv[]){
     
     else if(pidmainwc == 0)        //Wc exec
     {
-        // printf("First fork successful.\n");
-        // //Initialize first pipe, and fork to second process
-        // printf("First pipe, second fork...\n");
-        // if(pipe(fdwcgrep) < 0)
-        // {
-        //     perror("Pipe error, wc/grep.");
-        //     return -1;
-        // }
+        printf("First fork successful.\n");
+        //Initialize first pipe, and fork to second process
+        printf("First pipe, second fork...\n");
+        if(pipe(fdwcgrep) < 0)
+        {
+            perror("Pipe error, wc/grep.");
+            return -1;
+        }
 
-        // else if((pidwcgrep = fork()) < 0)
-        // {
-        //     perror("Fork error, wc/grep.");
-        //     return -1;
-        // }
+        else if((pidwcgrep = fork()) < 0)
+        {
+            perror("Fork error, wc/grep.");
+            return -1;
+        }
         
-        // else if(pidwcgrep == 0)         //Grep code
-        // {
-        //     printf("Successful.\n");
-        //     //Initialize second pipe, and fork to third process
-        //     printf("Second pipe, third fork...\n");
+        else if(pidwcgrep == 0)         //Grep code
+        {
+            printf("Successful.\n");
+            //Initialize second pipe, and fork to third process
+            printf("Second pipe, third fork...\n");
             if(pipe(fdgrepps) < 0)
             {
               perror("Pipe error, grep/ps.");
@@ -67,21 +67,21 @@ int main(int argc, char* argv[]){
                 printf("Executing grep argv[1]...\n");
                 dup2(fdgrepps[RD], STDIN_FILENO);
                 close(fdgrepps[WR]);
-                // dup2(fdwcgrep[WR], STDOUT_FILENO);
-                // close(fdwcgrep[RD]);
+                dup2(fdwcgrep[WR], STDOUT_FILENO);
+                close(fdwcgrep[RD]);
                 execlp("grep", "grep", argv[1], NULL);
             }
 
-        // }
+        }
 
-        // else                      //Wc exec
-        // {
-        //     wait(NULL);
-        //     printf("Executing wc -l...\n");
-        //     dup2(fdwcgrep[RD], STDOUT_FILENO);
-        //     close(fdwcgrep[WR]);
-        //     execlp("wc", "wc", "-l", NULL);
-        // }
+        else                      //Wc exec
+        {
+            wait(NULL);
+            printf("Executing wc -l...\n");
+            dup2(fdwcgrep[RD], STDIN_FILENO);
+            close(fdwcgrep[WR]);
+            execlp("wc", "wc", "-l", NULL);
+        }
     }
 
     else                     //Main exec
